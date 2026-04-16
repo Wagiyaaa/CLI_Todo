dd204ddf598ad861641c0aafc2bd6caf0399eb71
Wagiyaaa <jncdeguzman@gmail.com>
2026-04-16 18:56:08 +0800

    Created add command as well as initialization of tasks.json file

---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
+type Task struct {
+	ID   int    `json:"id"`
+	Text string `json:"text"`
+	Done bool   `json:"done"`
+}
+
+func marshal_json(tasks *[]Task) error {
+	jsonTask, json_encode_err := json.Marshal(&tasks)
+	if json_encode_err != nil {
+		fmt.Println(json_encode_err)
+		return json_encode_err
+	}
+	json_write_to_file_err := os.WriteFile("tasks.json", jsonTask, 0644)
+	if json_write_to_file_err != nil {
+		fmt.Println(json_write_to_file_err)
+		return json_write_to_file_err
+	}
+	return nil
+}
+
+func unmarshal_json(tasks *[]Task) error {
+	json_data, read_file_err := os.ReadFile("tasks.json")
+
+	if read_file_err != nil {
+		return read_file_err
+	}
+	unmarshal_err := json.Unmarshal(json_data, &tasks)
+	if unmarshal_err != nil {
+		return unmarshal_err
+	}
+	return nil
+}
+
+func add_task(text string) error {
+	var tasks []Task
+
+	unmarshal_err := unmarshal_json(&tasks)
+	if unmarshal_err != nil {
+		return unmarshal_err
+	}
+	
+	var id int
+	if len(tasks) == 0 {
+		id = 1
+	} else {
+		id = tasks[len(tasks)-1].ID + 1
+	}
+	task := Task{
+		ID:   id,
+		Text: text,
+		Done: false,
+	}
+
+	tasks = append(tasks, task)
+
+	err := marshal_json(&tasks)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func initialize_file() error {
+	if _, err := os.Stat("tasks.json"); err == nil {
+		return err
+	} else if errors.Is(err, os.ErrNotExist) {
+		err := os.WriteFile("tasks.json", []byte("[]"), 0644)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		return err
+	} else {
+		fmt.Println(err)
+		return err
+	}
+}
+
+func main() {
+	args := os.Args
+	if len(args) > 1 {
+		err := initialize_file()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		if args[1] == "add" && len(args) == 3 {
+			err := add_task(args[2])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			return
+		}
+	}
+	fmt.Println("No command specified...")
+}
